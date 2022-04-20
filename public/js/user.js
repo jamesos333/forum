b23553d81edd22d3adf25dbd09d4606c2f098e73
jamesos333 <osullivj2@newpaltz.edu>
2022-04-20 11:18:07 -0400

    restructed files and added node.js

---
+++ public/js/user.js
@@ -0,0 +1,36 @@
+//the user datatype for user data
+class User {
+    constructor (id, username, email, birthday, password) {
+        this.userId = id;
+        this.username = username;
+        this.email = email;
+        this.birthday = birthday;
+        this.password = password;
+    }
+    //getters
+    getId(){
+        return this.userId;
+    }
+    getUsername() {
+        return this.username;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getBirthday() {
+        return this.birthday;
+    }
+    //setters
+    setUsername(username) {
+        this.username = username;
+    }
+    setPassword(password) {
+        this.password = password;
+    }
+    setEmail(email) {
+        this.email = email;
+    }
+    setBirthday(birthday) {
+        this.birthday = birthday;
+    }
+}
